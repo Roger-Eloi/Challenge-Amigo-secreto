acc9d51cd3aa261247d1155484d532235c259e8a
Roger <rogereloi157@gmail.com>
2025-02-11 23:20:23 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+
+function adicionarAmigo(){
+    let entrada = document.getElementById("amigo").value;
+
+    if (entrada == ""){
+        return alert("Por favor, insira um nome.")
+    }
+
+    amigos.push(entrada)
+    console.log(amigos)
+    limparTela()
+}
+function sortearAmigo(){
+
+}
+
+
+function imprimirNaTela(){
+
+}
+function limparTela(){
+    document.
+}
